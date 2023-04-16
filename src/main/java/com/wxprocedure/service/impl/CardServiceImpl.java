2cf13830877e6b5ac3bd507d75d23bb0840c44ee
halo bear <49558643+jumtp@users.noreply.github.com>
2023-04-16 13:33:23 +0800

    card service finish

---
+++ src/main/java/com/wxprocedure/service/impl/CardServiceImpl.java
@@ -0,0 +1,21 @@
+package com.wxprocedure.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class CardServiceImpl {
+
+    public final List<String> data = Arrays.asList("card one", "acr t", "adfklj", "aew533");
+
+
+    public List<String> filter(String match) {
+        return data.stream().filter(item -> item.contains(match)).toList();
+    }
+
+    public List<String> all(){
+        return data;
+    }
+}
